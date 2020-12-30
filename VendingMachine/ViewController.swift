dbae5251a8c270c19d98e69cb417922b3097ba35
Onishi Ryosuke <onishi.appcreate@gmail.com>
2020-12-30 12:41:56 +0900

    Initial Commit

---
+++ VendingMachine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VendingMachine
+//
+//  Created by ryosuke on 2020/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
