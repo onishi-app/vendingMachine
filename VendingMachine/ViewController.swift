1c0cd24d2ae9008c060e6b6c0822f904cf0b24dc
Onishi Ryosuke <onishi.appcreate@gmail.com>
2020-12-30 13:53:48 +0900

    UIの実装
    UIを全て実装しました。
    また、IBActionで連携しました。

--- VendingMachine/ViewController.swift
+++ VendingMachine/ViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 所持金ラベル
+    @IBOutlet weak var moneyLabel: UILabel!
+    // 投入金額ラベル
+    @IBOutlet weak var inputMoneyLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // this is test 
-
+ 
+        
+    }
+    
+    // 購入ボタン
+    @IBAction func buyButton(_ sender: Any) {
+        
+    }
+    
+    // 投入額選択ボタン
+    @IBAction func sendMoney(_ sender: Any) {
+        
+    }
+    
+    // お釣りボタン
+    @IBAction func changeButton(_ sender: Any) {
+    
     }
 }
-
